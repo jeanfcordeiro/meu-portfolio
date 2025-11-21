975b516a3b21a95a9439b484691c768f6c49f174
Jean Felipe <jeanf13.jfc@gmail.com>
2025-11-21 09:23:35 -0300

    criação do hamburguer menu para telas menores

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,11 @@
-import { SquareMenuIcon } from 'lucide-react';
+// import { SquareMenuIcon } from 'lucide-react';
 import { Menu } from '../Menu';
 import styles from './styles.module.css';
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.header__content}>
-        <h2 className={styles.title}>Jean Felipe</h2>
-        <SquareMenuIcon className={styles.header__menu__icon} />
-      </div>
+      <h2 className={styles.title}>Jean Felipe</h2>
       <Menu />
     </header>
   );
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,49 @@
+import { useState } from 'react';
 import styles from './styles.module.css';
+import { SquareMenuIcon } from 'lucide-react';
 
 export function Menu() {
+  const [active, setMode] = useState(false);
+  const ToggleMode = () => {
+    setMode(!active);
+    console.log(active);
+  };
+
   return (
-    <nav className={styles.menu} role='navigation'>
-      <ul>
-        <li>
-          <a href='#'>Home</a>
-        </li>
-        <li>
-          <a href='#'>Projetos</a>
-        </li>
-        <li>
-          <a href='#'>Currículo</a>
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.content}>
+      <div
+        className={active ? `${styles.icon} ${styles.iconActive}` : styles.icon}
+        onClick={ToggleMode}
+      >
+        <SquareMenuIcon className={styles.header__menu__icon} />
+      </div>
+      <div
+        className={
+          active
+            ? `${styles.menu} ${styles.menuOpen}`
+            : `${styles.menu} ${styles.menuClose}`
+        }
+      >
+        <nav className={styles.list} role='navigation'>
+          <ul className={styles.listItems}>
+            <li>
+              <a className={styles.listLink} href='#'>
+                Home
+              </a>
+            </li>
+            <li>
+              <a className={styles.listLink} href='#'>
+                Projetos
+              </a>
+            </li>
+            <li>
+              <a className={styles.listLink} href='#'>
+                Currículo
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 }
