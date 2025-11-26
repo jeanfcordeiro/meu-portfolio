a6d74e73561932e50ab5c64576b2386b01a7e4c9
Jean Felipe <jeanf13.jfc@gmail.com>
2025-11-26 08:47:45 -0300

    criando componente GenericHtml

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,11 +1,22 @@
 import { Container } from '../../components/Container';
+import { GenericHtml } from '../../components/GenericHtml';
 import { MainTemplate } from '../../template/MainTemplate';
 
 export function NotFound() {
   return (
     <MainTemplate>
       <Container>
-        <h1>Página não encontrada</h1>
+        <GenericHtml>
+          <h1>404</h1>
+          <h2>Ops! Página não encontrada</h2>
+          <p>
+            A página que você está procurando pode ter sido removida ou o link
+            está incorreto.
+          </p>
+          <a href='/' className='btn'>
+            Voltar para o Início
+          </a>
+        </GenericHtml>
       </Container>
     </MainTemplate>
   );
